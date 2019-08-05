2b67cf8a01bef357e28d3a6cadacea1114b77627
Zuraitul Nasibah <zuraituln@gmail.com>
2019-08-05 22:10:55 +0800

    adding rtc

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,32 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+ timer: any;
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ionViewDidEnter() {
+    this.timer = setInterval(() => {
+      this.setData();
+    }, 1000);
+  }
+
+  setData(){
+
+let date = new Date().toLocaleDateString();
+
+let time = new Date().toLocaleTimeString();
+
+let both = date + ' ' + time;
+
+console.log(both);
+
+}
+
+ ionViewWillLeave() {
+    clearTimeout(this.timer);
   }
 
 }
